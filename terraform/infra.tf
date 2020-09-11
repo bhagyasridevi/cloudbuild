d93bde754bbe4a4ffe11c0df31e882470dad9fd2
bhagyasridevi <63953824+bhagyasridevi@users.noreply.github.com>
2020-09-11 12:52:43 +0530

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -3,8 +3,8 @@ provider "google" {
   region = "us-central1"
 }
 
-resource "google_storage_bucket" "bucketterra234" {
-  name = "batch-pipeline12"
+resource "google_storage_bucket" "buckett" {
+  name = "batch-pipeline"
   storage_class = "REGIONAL"
   location  = "us-central1"
 }
