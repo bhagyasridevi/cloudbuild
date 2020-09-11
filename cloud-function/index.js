3c1bba91e43dd201a51153acf326b216011eb35c
bhagyasridevi <63953824+bhagyasridevi@users.noreply.github.com>
2020-09-11 11:17:45 +0530

    Update index.js

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -1,27 +1,51 @@
-const expect = require('chai').expect
-const index = require('./index')
+//gcloud --project=grey-sort-challenge functions deploy goWithTheDataFlow --stage-bucket gs://batch-pipeline --trigger-bucket gs://batch-pipeline
 const google = require('googleapis');
+exports.goWithTheDataFlow = function(event, callback) {
+  const file = event.data;
+  const context = event.context;
 
-describe('The cloud function', function () {
-  it('Check eventType & file path', function * () {
+  console.log("File is: ", file);
+  console.log("State is: ", context.eventType);
 
-      const event = {
-        data: {
-          bucket: "gs://batch-pipeline",
-          name: "upload/filename"
-        },
-        context: {
-          eventType: "google.storage.object.finalize"
+  if (context.eventType === 'google.storage.object.finalize' && file.name.indexOf('upload/') !== -1) {
+    google.auth.getApplicationDefault(function (err, authClient) {
+      if (err) {
+        throw err;
+      }
+      // See https://cloud.google.com/compute/docs/authentication for more information on scopes
+      if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+        // Scopes can be specified either as an array or as a single, space-delimited string.
+        authClient = authClient.createScoped([
+          'https://www.googleapis.com/auth/cloud-platform',
+          'https://www.googleapis.com/auth/userinfo.email'
+        ]);
+      }
+      google.auth.getDefaultProjectId(function(err, projectId) {
+        if (err || !projectId) {
+          console.error(`Problems getting projectId (${projectId}). Err was: `, err);
+          throw err;
         }
-      };
-
-    const getApplicationDefaultStub = this.sandbox.stub(google.auth, 'getApplicationDefault');
-
-    // Call tested function and verify its behavior
-    const result = index.goWithTheDataFlow(event, () => {
-      t.end();
+        const dataflow = google.dataflow({ version: 'v1b3', auth: authClient });
+        dataflow.projects.templates.create({
+          projectId: projectId,
+          resource: {
+            parameters: {
+              inputFile: `gs://${file.bucket}/${file.name}`
+            },
+            jobName: 'called-from-a-cloud-function-batch-pipeline-' + new Date().getTime(),
+            gcsPath: 'gs://batch-pipeline/template/pipeline'
+          }
+        }, function(err, response) {
+          if (err) {
+            console.error("Problem running dataflow template, error was: ", err);
+          }
+          console.log("Dataflow template response: ", response);
+          callback();
+        });
+      });
     });
-
-    expect(getApplicationDefaultStub).to.be.calledWith()
-  })
-})
+  } else {
+    console.log("Nothing to do here, ignoring.");
+    callback();
+  }
+};
